47ab225e4d16cd74d1125aa4289c2938fbcf8cd8
iefan <mybsppp@163.com>
2019-05-02 15:23:23 +0800

    统计小写字母

--- di56tian.py
+++ di56tian.py
@@ -25,6 +25,22 @@ def shuchuzifuchuangeshu():
     print("字符个数：{}".format(num))
     print("点('.')的个数：{}".format(numa))
 
+def shuxiaoxiezimudegeshu():
+    num=[0]*26
+    ch1=input()
+    i=0
+    while i<len(ch1):
+        if ch1[i]>='a' and ch1[i]<='z':
+            k=ord(ch1[i])-ord('a')
+            num[k]+=1
+        i+=1
+    for i in range(0,26):
+        ch2=chr(97+i)
+        print(ch2,':',num[i],end='  ',sep='')
+        if i%5==4:
+            print()
+
 if __name__=="__main__":
-    shuchuzifuchuangeshu()
+    shuxiaoxiezimudegeshu()
+    # shuchuzifuchuangeshu()
     # xuanzepaixu()
